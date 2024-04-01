b91b2182ecd271d72b37e990150201b3a2d6c4c5
Denis Roenko <d.roenko@corp.mail.ru>
2024-04-01 09:16:02 +0300

    Initial Commit

---
+++ Rectangle_problem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rectangle_problem
+//
+//  Created by d-roenko on 01.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
